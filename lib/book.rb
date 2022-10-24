71d74113a5d1678f70ebb438c193feefc6fd8440
Masai11 <roy.kiplagat@student.moringaschool.com>
2022-10-24 17:44:17 +0300

    commit tree 372775ecb1460f042efc7442e7766bf3d62a1580

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,14 @@
+class Book
+    attr_accessor :title
+    attr_reader :title
+    attr_reader :athor
+    attr_reader :page_count
+    attr_reader :genre
+    
+    def initialize(title,author_name,page_count,genre)
+        @title = title
+    end
 
+end
+title = Book.new("And Then There Were None")
 
